36344a17431cce40b5fc7990191774250b898cca
George Offley <goffley3@aol.com>
2013-10-28 13:40:17 -0400

    added img for base, tried to configure uploading

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-var myPackage = angular.module('myPackage', []);
+angular.module('package').controller('AppCtrl', function ($scope, upload) {
+  $scope.doUpload = function () {
+    upload({
+      url: '/upload',
+      data: {
+        anint: 123,
+        aBlob: Blob([1,2,3]), // Only works in newer browsers
+        aFile: $scope.myFile, // a jqLite type="file" element, upload() will extract all the files from the input and put them into the FormData object before sending.
+      }
+    }).then(
+      function (response) {
+        console.log(response.data); // will output whatever you choose to return from the server on a successful upload
+      },
+      function (response) {
+          console.error(response); //  Will return if status code is above 200 and lower than 300, same as $http
+      }
+    );
+  }
+});
